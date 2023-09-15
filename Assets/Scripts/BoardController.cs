08d5927b9c36ede57b496f9b0bb62d7bacef960c
Sergi <torryxd@gmail.com>
2023-09-15 13:30:34 +0200

    Fichas y tablero con marcas

--- Assets/Scripts/BoardController.cs
+++ Assets/Scripts/BoardController.cs
@@ -5,44 +5,65 @@ using UnityEngine;
 
 public class BoardController : MonoBehaviour
 {
-    [SerializeField]
-    private int _size;
+    public int size;
+    public TileType turnType;
+
     [SerializeField]
     private Tile _tilePrefab;
     [SerializeField]
     private Transform _cam;
 
-    private Dictionary<Vector2, Tile> _tiles;
+    public Dictionary<Vector2, Tile> tiles;
 
 
     private void Start()
     {
         GenerateGrid();
+
+        turnType = TileType.Black;
     }
 
     private void GenerateGrid()
     {
-        _tiles = new Dictionary<Vector2, Tile>();
-        for (int x = 0; x < _size; x++)
+        tiles = new Dictionary<Vector2, Tile>();
+        for (int x = 0; x <= (size + 1); x++)
         {
-            for (int y = 0; y < _size; y++)
+            for (int y = 0; y <= (size + 1); y++)
             {
                 var spawnedTile = Instantiate(_tilePrefab, new Vector3(x, y), Quaternion.identity);
-                spawnedTile.name = $"Tile {x} {y}";
+                spawnedTile.name = $"Tile[{NumberToLetter(x)},{NumberToLetter(y)}]";
 
-                // var tileIsBlackInChess = (x % 2 == 0 && y % 2 != 0) || (x % 2 != 0 && y % 2 == 0);
+                Vector2 boardPosition = new Vector2(x, y);
+                tiles[boardPosition] = spawnedTile;
 
-                _tiles[new Vector2(x, y)] = spawnedTile;
+                spawnedTile.Init(this, boardPosition);
             }
         }
 
-        _cam.transform.position = new Vector3((float)_size / 2 - 0.5f, (float)_size / 2 - 0.5f, -10);
+        _cam.transform.position = new Vector3((float)size / 2 + 0.5f, (float)size / 2 + 0.5f, -10);
     }
 
     public Tile GetTileAtPosition(Vector2 pos)
     {
-        if (_tiles.TryGetValue(pos, out var tile))
+        if (tiles.TryGetValue(pos, out var tile))
             return tile;
         return null;
+    }
+
+    public char NumberToLetter(int i)
+    {
+        return Convert.ToChar(64 + i);
+    }
+
+    public void NextTurn()
+    {
+        if(turnType == TileType.Black)
+        {
+            turnType = TileType.White;
+        }
+        else if(turnType == TileType.White)
+        {
+            turnType = TileType.Black;
+        }
     }
 }
--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -1,20 +1,138 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
+
+public enum TileType
+{
+    Border      = -1,
+    Liberty     =  0,
+    Black       =  1,
+    White       =  2,
+}
 
 public class Tile : MonoBehaviour
 {
+    public TileType type;
+
     [SerializeField]
     private GameObject _highlight;
+    [SerializeField]
+    private Transform _stonesParent;
+    [SerializeField]
+    private Transform _tilesParent;
+    [SerializeField]
+    private Transform _outBordersParent;
+
+    private BoardController _board;
+    private Vector2 _boardPos;
 
 
-    void OnMouseEnter()
+    public void Init(BoardController board, Vector2 boardPos)
+    {
+        _board = board;
+        _boardPos = boardPos;
+
+        CheckTileBorder();
+        PlaceStone();
+    }
+
+    private void OnMouseEnter()
     {
         _highlight.SetActive(true);
     }
 
-    void OnMouseExit()
+    private void OnMouseExit()
     {
         _highlight.SetActive(false);
+    }
+
+    private void OnMouseDown()
+    {
+        if(PlaceStone())
+        {
+            _board.NextTurn();
+        }
+    }
+
+    public bool PlaceStone()
+    {
+        bool nextTurn = false;
+
+        if(type == TileType.Black || type == TileType.White)
+        {
+            type = TileType.Liberty;
+        }
+        else if (type == TileType.Liberty)
+        {
+            type = _board.turnType;
+            nextTurn = true;
+        }
+
+        for (int i = 0; i <= 2; i++)
+        {
+            _stonesParent.GetChild(i).gameObject.SetActive(i == (int)type);
+        }
+
+        return nextTurn;
+    }
+
+    private void CheckTileBorder()
+    {
+        if (_boardPos.x > _board.size || _boardPos.y > _board.size || _boardPos.x < 1 || _boardPos.y < 1)
+        {
+            type = TileType.Border;
+            int borderPosition = GetOutBorderPosition(_boardPos, 0, _board.size + 1);
+            for (int i = 1; i <= 9; i++)
+            {
+                _outBordersParent.GetChild(i - 1).gameObject.SetActive(i == borderPosition);
+            }
+        }
+        else
+        {
+            type = TileType.Liberty;
+            _outBordersParent.gameObject.SetActive(false);
+
+            int tilePosition = GetOutBorderPosition(_boardPos, 1, _board.size);
+            for (int i = 1; i <= 9; i++)
+            {
+                _tilesParent.GetChild(i - 1).gameObject.SetActive(i == tilePosition);
+            }
+        }
+    }
+
+    private int GetOutBorderPosition(Vector2 pos, int boardStart, int boardEnd)
+    {
+        // 1 2 3
+        // 4 5 6
+        // 7 8 9
+
+        if (pos.x <= boardStart)
+        {
+            if (pos.y <= boardStart)
+                return 7;
+            else if (pos.y >= boardEnd)
+                return 1;
+            else
+                return 4;
+        }
+        else if (pos.x >= boardEnd)
+        {
+            if (pos.y <= boardStart)
+                return 9;
+            else if (pos.y >= boardEnd)
+                return 3;
+            else
+                return 6;
+        }
+        else
+        {
+            if (pos.y <= boardStart)
+                return 8;
+            else if (pos.y >= boardEnd)
+                return 2;
+            else
+                return 5;
+        }
     }
 }
